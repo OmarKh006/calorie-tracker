e04f33d24053c9502e12664e982b382403256a43
OmarKh006 <khairyomar006@gmail.com>
2026-04-25 07:47:23 +0300

    first component

---
+++ src/components/CalorieRecord.jsx
@@ -0,0 +1,16 @@
+import "./CalorieRecord.css";
+
+function CalorieRecord() {
+  return (
+    <>
+      <ul className="record">
+        <li>25/4/2026</li>
+        <li>Breakfast</li>
+        <li>Molasses and Tahina</li>
+        <li className="record-calories">340</li>
+      </ul>
+    </>
+  );
+}
+
+export default CalorieRecord;
