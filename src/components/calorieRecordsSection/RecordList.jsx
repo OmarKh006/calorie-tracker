3389f135a7e74ffff91cba899df0842aafd3d5bb
OmarKh006 <khairyomar006@gmail.com>
2026-04-28 09:35:47 +0300

    dynamic styling using css modules

--- src/components/calorieRecordsSection/RecordList.jsx
+++ src/components/calorieRecordsSection/RecordList.jsx
@@ -3,10 +3,10 @@ import styles from "./RecordList.module.css";
 
 export default function RecordList(props) {
   return (
-    <ul className={styles.recordList}>
+    <ul className={styles["record-list"]}>
       {props.records.map((record) => {
         return (
-          <li className={styles.listItem} key={record.id}>
+          <li className={styles["list-item"]} key={record.id}>
             <CalorieRecord record={record}></CalorieRecord>
           </li>
         );
