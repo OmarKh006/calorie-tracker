c737a94d11428365c4ad9518d05b1a3a84379fd1
OmarKh006 <khairyomar006@gmail.com>
2026-05-11 13:51:08 +0300

    Defining react router

---
+++ src/components/pages/LandingPage.jsx
@@ -0,0 +1,8 @@
+export const LandingPage = () => {
+  return (
+    <>
+      <p>Welcome to calorie tracker app</p>
+      <p>Get Started</p>
+    </>
+  );
+};
---
+++ src/components/pages/TrackPage.jsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react";
+import CaloriesRecordEdit from "../edit/CaloriesRecordEdit";
+import ListingSection from "../calorieRecordsSection/ListingSection";
+import styles from "./TrackPage.module.css";
+import ReactModal from "react-modal";
+import AppContextProvider from "../../AppContext";
+
+const LOCAL_STORAGE_KEY = "calorieRecords";
+
+export function TrackPage() {
+  const [records, setRecords] = useState();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const modalStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      border: "none",
+      borderRadius: "var(--form-border-radius)",
+      padding: "0px",
+    },
+    overlay: {
+      backgroundColor: "rgba(0,0,0,0.5)",
+    },
+  };
+
+  function saveToLocalStorage() {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(records));
+  }
+
+  function loadFromLocalStorage() {
+    const loadedRecords = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (loadedRecords != null && loadedRecords !== "undefined")
+      setRecords(
+        JSON.parse(loadedRecords).map((record) => ({
+          ...record,
+          date: new Date(record.date),
+        })),
+      );
+    else setRecords([]);
+  }
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const formSubmit = (record) => {
+    const formattedRecord = {
+      ...record,
+      date: record.date,
+      id: crypto.randomUUID(),
+    };
+    setRecords((prevRecord) => [formattedRecord, ...(prevRecord ?? [])]);
+    handleCloseModal();
+  };
+
+  useEffect(() => {
+    if (!records) loadFromLocalStorage();
+    else saveToLocalStorage();
+  }, [records]);
+
+  return (
+    <>
+      <h1 className={styles.title}>Calories Tracker</h1>
+      <AppContextProvider>
+        <ReactModal
+          isOpen={isModalOpen}
+          onRequestClose={handleCloseModal}
+          contentLabel="ReactModal"
+          style={modalStyles}
+        >
+          <CaloriesRecordEdit
+            onFormSubmit={formSubmit}
+            onCancel={handleCloseModal}
+          />
+        </ReactModal>
+        {records && <ListingSection allRecords={records} />}
+      </AppContextProvider>
+      <button className={styles["open-modal-button"]} onClick={handleOpenModal}>
+        Track Food
+      </button>
+    </>
+  );
+}
