a87673a2626568fde4106e9415e6264b039afa76
OmarKh006 <khairyomar006@gmail.com>
2026-05-11 17:52:15 +0300

    auto redirect from error page to home page after 10s

--- src/components/pages/ErrorPage.jsx
+++ src/components/pages/ErrorPage.jsx
@@ -1,10 +1,37 @@
-import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const REDIRECT_COUNT = 10;
+const COUNT_DOWN_INTERVAL = 1000;
+const HOME_LINK = "/";
 
 export const ErrorPage = () => {
+  const [counter, setCounter] = useState(REDIRECT_COUNT);
+  const intervalHandler = useRef();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    intervalHandler.current = setInterval(() => {
+      setCounter((prev) => prev - 1);
+    }, COUNT_DOWN_INTERVAL);
+
+    return () => clearInterval(intervalHandler.current);
+  }, []);
+
+  useEffect(() => {
+    if (counter === 0) {
+      clearInterval(intervalHandler.current);
+      navigate(HOME_LINK);
+    }
+  }, [counter]);
+
   return (
     <>
       <p>Something went wrong</p>
-      <Link to="/">Redirect to HomePage</Link>
+      <p>Redirecting to home page in {counter}s</p>
+      <p>
+        Or click <Link to={HOME_LINK}>here</Link> to redirect to Home Page
+      </p>
     </>
   );
 };
